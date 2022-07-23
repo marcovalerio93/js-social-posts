47de6af5b7ab98517aea9e569602191512eca6d9
Marco Valerio <mo.valerio93@gmal.com>
2022-07-23 14:30:50 +0200

    social post milestone 2 done

--- main.js
+++ main.js
@@ -55,3 +55,38 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+const containerDom = document.getElementById('container');
+
+posts.forEach( post => {
+    containerDom.innerHTML += `<div class="post">
+                                    <div class="post__header">
+                                        <div class="post-meta">                    
+                                            <div class="post-meta__icon">
+                                                <img class="profile-pic" src="${post.author.image}" alt="${post.author.name}>                    
+                                            </div>
+                                            <div class="post-meta__data">
+                                                <div class="post-meta__author">${post.author.name}</div>
+                                                <div class="post-meta__time">${post.created}</div>
+                                            </div>                    
+                                        </div>
+                                    </div>
+                                    <div class="post__text">${post.content}</div>
+                                    <div class="post__image">
+                                        <img src="${post.media}" alt="">
+                                    </div>
+                                    <div class="post__footer">
+                                        <div class="likes js-likes">
+                                            <div class="likes__cta">
+                                                <a class="like-button  js-like-button" href="#" data-postid="${post.id}">
+                                                    <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                                                    <span class="like-button__label">Mi Piace</span>
+                                                </a>
+                                            </div>
+                                            <div class="likes__counter">
+                                                Piace a <b id="like-counter-${post.id}" class="js-likes-counter">${post.like}</b> persone
+                                            </div>
+                                        </div> 
+                                    </div>            
+                                </div>`;
+})
